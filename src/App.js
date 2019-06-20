2a248eec15552787779e61dbc2c6ee9976516ae2
Abdulrafayzahid <45996454+Abdulrafayzahid@users.noreply.github.com>
2019-06-20 14:28:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,33 +6,6 @@ import * as BookApi from "./BookApi";
 import SearchPage from "./SearchPage";
 import BooksPage from "./BooksPage";
 
-
-// function App() {
-//   console.log(BookApi.search('react'))
-//   console.log(BookApi.getAll())
-
-//   return (
-//     <div className="App">
-//       {/* <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> */}
-
-//     </div>
-//   );
-// }
-
-//export default App;
 class App extends Component {
   state = {
     books : []
@@ -48,9 +21,9 @@ class App extends Component {
   BookApi.getAll()
   }
   render() {
-  console.log(
-   BookApi.getAll()
-  );
+//   console.log(
+//    BookApi.getAll()
+//   );
 
 
     const updateShelf = (books, shelf) => {
