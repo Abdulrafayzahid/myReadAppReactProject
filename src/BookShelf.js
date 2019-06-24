835d98d3b328b079f1573d85c50a02c00c8554a7
Abdulrafayzahid <45996454+Abdulrafayzahid@users.noreply.github.com>
2019-06-24 19:43:30 +0500

    Update BookShelf.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import Book from './book';
 
-const BookShelf = (props)=> { 
-        return (  <div className="list-books-content">
-        <div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">{props.currentShelf}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                  {props.book.map(book => (
-                    <li key={book.id}>
-                      <Book
-                        book={book}
-                        updateShelf={props.updateShelf}
-                        currentShelf={props.currentShelf}
-                      />
-                    </li>
-                  ))}
-              </ol>
-            </div>
-          </div>
-       </div>
-       </div> );
-    }
 
+const BookShelf = (props) => {
+        return ( 
+        <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.book
+            .map(book => (
+              <li key={book.id}>
+                <Book
+                  book={book}
+                  updateShelf= {props.updateShelf}
+                  currentShelf={props.currentShelf}
+                />
+              </li>
+            ))}
+        </ol>
+        </div> );
+}
  
 export default BookShelf;
